8a8131c16280b1c8b5c872b05362312d5fc8d1b9
Reynerio Samos <reynerio.samos@gmail.com>
2023-10-05 12:25:07 -0600

    first commit

---
+++ Prog2.nim
@@ -0,0 +1,13 @@
+#Programming Languages Project 2 - Group 3
+# [Desc. of Program Goes Here]
+#[
+    Group 3 Members:
+        Javier Castellanos  - 2020253542
+        Tamika Chen         - 2019120211
+        Reynerio Samos      - 2018119235
+        Cahlil Tillet       - 2021154337
+        Joshua Richards     - 2017115716
+]#
+# Program was writtin using Nim Compiler Version 2.0.0 [Linux: amd64]
+
+echo "I hope you love BNF"
